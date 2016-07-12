632a4961ce71fd3ee66f193b8efd7d6f7c873eec
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 20:17:52 +0200

    Add IStructNode and IConstantNode interfaces for the Component Design Pattern.

---
+++ TK.JSONParser/Parsing/Nodes/IStructNode.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TK.JSONParser.Parsing.Nodes
+{
+    public interface IStructNode : INode
+    {
+        IReadOnlyCollection<INode> Items { get; }
+    }
+}
