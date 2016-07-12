d7dce69cf46cdb3c5ba63b8057444c2faca4b775
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 22:59:20 +0200

    Add the InlineVisitor to render a JSON AST into an in line string.

---
+++ TK.JSONParser.Tests/Visitors/InlineVisitorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TK.JSONParser.Parsing;
+using TK.JSONParser.Tokens;
+using TK.JSONParser.Visitors;
+
+namespace TK.JSONParser.Tests.Visitors
+{
+    [TestFixture]
+    public class InlineVisitorTests
+    {
+        [Test]
+        public void empty() { }
+
+        [TestCase(@"[]", @"[]")]
+        [TestCase(@"[123]", @"[ 123 ]")]
+        [TestCase(@"[123,456]", @"[ 123, 456 ]")]
+        [TestCase(@"{}", @"{}")]
+        [TestCase(@"{""p"":""123""}", @"{ ""p"" : ""123"" }")]
+        [TestCase(@"{""p"":""123"",""p2"":""456""}", @"{ ""p"" : ""123"", ""p2"" : ""456"" }")]
+        public void visitor_should_render_contant_values(string input, string expected)
+        {
+            var parser = new Parser(input);
+            INode node = parser.ParseJSON();
+            var visitor = new InlineVisitor();
+
+            var result = visitor.Visit(node);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
--- TK.JSONParser/Visitors/InlineVisitor.cs
+++ TK.JSONParser/Visitors/InlineVisitor.cs
@@ -10,34 +10,60 @@ namespace TK.JSONParser.Visitors
 {
     public class InlineVisitor : IVisitor<string>
     {
-        public string Visit(ObjectNode expression)
+        public string Visit(ArrayNode node)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder("[");
+
+            foreach (var item in node.Items)
+            {
+                sb.Append(" ");
+                sb.Append(item.Accept(this));
+                sb.Append(",");
+            }
+
+            if (sb.Length > 1)
+                sb.Replace(',', ' ', sb.Length - 1, 1);
+            sb.Append("]");
+
+            return sb.ToString();
         }
 
-        public string Visit(NumberNode expression)
+        public string Visit(ObjectNode node)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder("{");
+
+            foreach (var item in node.Items)
+            {
+                sb.Append(" ");
+                sb.Append(item.Accept(this));
+                sb.Append(",");
+            }
+
+            if (sb.Length > 1)
+                sb.Replace(',', ' ', sb.Length - 1, 1);
+            sb.Append("}");
+
+            return sb.ToString();
         }
 
         public string Visit(KeyValueNode node)
         {
-            throw new NotImplementedException();
+            return string.Format("{0} : {1}", node.Key.Accept(this), node.Value.Accept(this));
         }
 
-        public string Visit(StringNode expression)
+        public string Visit(NumberNode node)
         {
-            throw new NotImplementedException();
+            return node.Value.ToString();
         }
 
-        public string Visit(ArrayNode expression)
+        public string Visit(StringNode node)
         {
-            throw new NotImplementedException();
+            return string.Format("\"{0}\"", node.Value);
         }
 
-        public string Visit(ErrorNode expression)
+        public string Visit(ErrorNode node)
         {
-            throw new NotImplementedException();
+            return node.ToString();
         }
     }
 }
