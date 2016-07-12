b01ee3007eabada4df4f1c48370e8e6088cd2f0e
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 10:06:11 +0200

    Setup the Tests project.

---
+++ TK.JSONParser/TK.JSONParser.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TK.JSONParser.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void it_should_fail()
+        {
+            Assert.That(true, Is.False);
+        }
+    }
+}
