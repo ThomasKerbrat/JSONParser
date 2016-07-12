8cda634b147d2756c8d123b92f992abf767a053b
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 20:17:24 +0200

    Remove the TK.JSONParser.Parsing.Values namepsace.

--- TK.JSONParser/Parsing/Nodes/ArrayNode.cs
+++ TK.JSONParser/Parsing/Nodes/ArrayNode.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TK.JSONParser.Parsing.Nodes;
 using TK.JSONParser.Visitors;
 
 namespace TK.JSONParser.Parsing.Nodes
