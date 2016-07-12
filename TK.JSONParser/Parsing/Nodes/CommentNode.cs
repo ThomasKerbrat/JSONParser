1280db691ace315b39cc652beae3ee786740b6c1
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 23:47:18 +0200

    Add ICommentable interface and CommentNode class.

---
+++ TK.JSONParser/Parsing/Nodes/CommentNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TK.JSONParser.Visitors;
+
+namespace TK.JSONParser.Parsing.Nodes
+{
+    public class CommentNode : INode, IConstantNode<string>
+    {
+        private string comment;
+
+        public CommentNode(string comment)
+        {
+            this.comment = comment;
+        }
+
+        public string Value
+            => comment;
+
+        public T Accept<T>(IVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
