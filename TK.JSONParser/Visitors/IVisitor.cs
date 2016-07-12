4dc099b02edba38a081eaef8a1f078e13263702c
Thomas Kerbrat <kerbrat@outlook.com>
2016-07-12 19:40:41 +0200

    Add InlineVisitor for testing against strings instead of class instances.

---
+++ TK.JSONParser/Visitors/IVisitor.cs
@@ -0,0 +1,20 @@
+﻿using TK.JSONParser.Parsing.Expressions;
+using TK.JSONParser.Parsing.Values;
+
+namespace TK.JSONParser.Visitors
+{
+    public interface IVisitor<T>
+    {
+        T Visit(ArrayExpression expression);
+
+        T Visit(ObjectExpression expression);
+
+        T Visit(MemberExpression expression);
+
+        T Visit(NumberExpression expression);
+
+        T Visit(StringExpression expression);
+
+        T Visit(ErrorExpression expression);
+    }
+}
